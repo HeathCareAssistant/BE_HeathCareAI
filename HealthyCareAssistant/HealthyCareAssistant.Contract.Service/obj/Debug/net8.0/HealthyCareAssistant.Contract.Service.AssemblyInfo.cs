281312f1cee30b107c068f048814173f8beef913
letranminhdat1516 <datltmse170095@fpt.edu.vn>
2025-03-06 21:38:56 +0700

    fix conflict

--- HealthyCareAssistant/HealthyCareAssistant.Contract.Service/obj/Debug/net8.0/HealthyCareAssistant.Contract.Service.AssemblyInfo.cs
+++ HealthyCareAssistant/HealthyCareAssistant.Contract.Service/obj/Debug/net8.0/HealthyCareAssistant.Contract.Service.AssemblyInfo.cs
@@ -14,15 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthyCareAssistant.Contract.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f9c62207fcacba09e7a60e5003345c35a4fe92e")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aebf40ea73f25ddd40b317f074cce52e8759635")]
->>>>>>> 9392f0827b9628464c9489b67a86950ec6ff26c0
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab497cae2519f93c1d1b7f407e53edf1d908dbcc")]
->>>>>>> main
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a32f869329f6e653ae018cf4c4bf377860d2d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthyCareAssistant.Contract.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthyCareAssistant.Contract.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HealthyCareAssistant/HealthyCareAssistant.ModelViews/UserModelViews/UserModelView.cs
+++ HealthyCareAssistant/HealthyCareAssistant.ModelViews/UserModelViews/UserModelView.cs
@@ -3,36 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-<<<<<<< HEAD
-=======
 using HealthyCareAssistant.Contact.Repo.Entity;
->>>>>>> main
 
 namespace HealthyCareAssistant.ModelViews.UserModelViews
 {
     public class UserModelView
     {
-<<<<<<< HEAD
-        public int UserId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Fcmtoken { get; set; }
-        public DateTime? CreatedAt { get; set; }
-=======
-        public string Name { get; set; }
-        public string Email { get; set; }
         public object UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
         public object PhoneNumber { get; set; }
         public string? Role { get; set; }
-        public object CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
         public object UpdatedAt { get; set; }
+        public string Fcmtoken { get; set; }
 
         public UserModelView(User user)
         {
+            UserId = user.UserId;
             Name = user.Name;
             Email = user.Email;
+            PhoneNumber = user.PhoneNumber;
+            Role = user.Role?.RoleName ?? "User"; // Lấy Role nếu có
+            CreatedAt = user.CreatedAt;
+            UpdatedAt = user.UpdatedAt;
+            Fcmtoken = user.Fcmtoken;
         }
->>>>>>> main
+        public UserModelView() { }
     }
 }

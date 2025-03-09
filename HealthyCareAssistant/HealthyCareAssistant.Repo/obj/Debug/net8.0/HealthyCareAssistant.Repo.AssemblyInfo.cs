62078f6043d89cd45dabf46804d7d9b00769ce94
letranminhdat1516 <datltmse170095@fpt.edu.vn>
2025-03-10 06:55:29 +0700

    add login google

--- HealthyCareAssistant/HealthyCareAssistant.Repo/obj/Debug/net8.0/HealthyCareAssistant.Repo.AssemblyInfo.cs
+++ HealthyCareAssistant/HealthyCareAssistant.Repo/obj/Debug/net8.0/HealthyCareAssistant.Repo.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthyCareAssistant.Repo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0622b7705956fc48d6931807f62459d0d5cada9f")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f072a36d0cad29facf9845d19a78d7bab7efafe2")]
+>>>>>>> 9b85613 (add login google)
 [assembly: System.Reflection.AssemblyProductAttribute("HealthyCareAssistant.Repo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthyCareAssistant.Repo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

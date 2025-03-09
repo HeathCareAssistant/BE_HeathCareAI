9b85613d9ea1664f3a9df156fa3a8aac7ce1fdad
letranminhdat1516 <datltmse170095@fpt.edu.vn>
2025-03-10 06:55:29 +0700

    add login google

---
+++ HealthyCareAssistant/HealthyCareAssistant.Service/Service/firebase/FirebaseAuthService.cs
@@ -0,0 +1,53 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Auth;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HealthyCareAssistant.Service.Service.firebase
+{
+    public class FirebaseAuthService
+    {
+        private readonly FirebaseApp _firebaseApp;
+
+        public FirebaseAuthService(IConfiguration configuration)
+        {
+            var serviceAccountPath = configuration["Firebase:ServiceAccountPath"];
+            var fullPath = Path.Combine(Directory.GetCurrentDirectory(), serviceAccountPath);
+
+            if (!File.Exists(fullPath))
+            {
+                throw new FileNotFoundException($"Không tìm thấy file Firebase JSON: {fullPath}");
+            }
+
+            if (FirebaseApp.DefaultInstance == null)
+            {
+                _firebaseApp = FirebaseApp.Create(new AppOptions
+                {
+                    Credential = GoogleCredential.FromFile(fullPath)
+                });
+            }
+            else
+            {
+                _firebaseApp = FirebaseApp.DefaultInstance;
+            }
+        }
+
+        /// <summary>
+        /// Xác thực ID Token với Firebase
+        /// </summary>
+        public async Task<FirebaseToken> VerifyIdTokenAsync(string idToken)
+        {
+            try
+            {
+                return await FirebaseAuth.DefaultInstance.VerifyIdTokenAsync(idToken);
+            }
+            catch (FirebaseAuthException ex)
+            {
+                throw new UnauthorizedAccessException("Token không hợp lệ", ex);
+            }
+        }
+    }
+}

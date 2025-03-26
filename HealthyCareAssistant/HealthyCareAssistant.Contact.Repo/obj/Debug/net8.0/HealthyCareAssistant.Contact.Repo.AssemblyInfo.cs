08c3b2c89f778e8911a79ef258a6f59138856ef8
letranminhdat1516 <datltmse170095@fpt.edu.vn>
2025-03-26 14:16:52 +0700

    fix

--- HealthyCareAssistant/HealthyCareAssistant.Contact.Repo/obj/Debug/net8.0/HealthyCareAssistant.Contact.Repo.AssemblyInfo.cs
+++ HealthyCareAssistant/HealthyCareAssistant.Contact.Repo/obj/Debug/net8.0/HealthyCareAssistant.Contact.Repo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthyCareAssistant.Contact.Repo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b594ba66d63b2c92144fb728b8928ad8f2c9f31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab10ffc353b81160d353cc32616369995389c87f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthyCareAssistant.Contact.Repo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthyCareAssistant.Contact.Repo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HealthyCareAssistant/HealthyCareAssistant.Service/obj/Debug/net8.0/HealthyCareAssistant.Service.AssemblyInfo.cs
+++ HealthyCareAssistant/HealthyCareAssistant.Service/obj/Debug/net8.0/HealthyCareAssistant.Service.AssemblyInfo.cs
@@ -15,8 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f072a36d0cad29facf9845d19a78d7bab7efafe2")]
 =======
+=======
+>>>>>>> Stashed changes
 <<<<<<< HEAD
 <<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6a6b6df38a38aa44593b11d8e3cba49c0449ea")]
@@ -26,6 +29,9 @@ using System.Reflection;
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c33c60d72c5f0a37da8769b07436965c870903e")]
 >>>>>>> 23c07a1f76d014faf8df54e413d12f4cac51d327
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("HealthyCareAssistant.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthyCareAssistant.Service")]
